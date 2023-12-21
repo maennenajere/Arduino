0daa906dda8941a1fe92c73fa4238294ba64014c
Jere <122638516+maennenajere@users.noreply.github.com>
2023-12-21 16:50:45 +0200

    Update Arduino_vko.ino

--- Arduino_vko.ino
+++ Arduino_vko.ino
@@ -1,42 +1,39 @@
-// Tämä Arduino-skripti lukee arvoja neljästä analogisesta pinnistä ja tulostaa ne sarjaporttiin.
-// Sitä käytetään erilaisten sensorien (ovi, ikkuna, liike) ja hälytyksen seurantaan.
+// This Arduino script reads values from four analog pins and prints them to the serial port.
+// It is used to monitor various sensors (door, window, motion) and an alarm system.
 
 // These constants won't change. They're used to give names to the pins used:
-const int analogInPin0 = A0;  //kerrotaan ohjelmalle, mitä pinnejä käytetään 
-const int analogInPin1 = A1; 
-const int analogInPin2 = A2;
-const int analogInPin3 = A3;
+const int analogInPin0 = A0;  // Pin A0 used for reading
+const int analogInPin1 = A1;  // Pin A1 used for reading
+const int analogInPin2 = A2;  // Pin A2 used for reading
+const int analogInPin3 = A3;  // Pin A3 used for reading
 
-int ovisensori = 0;    //nimetään muuttujat    
-int ikkunasensori = 0; 
-int liikesensori = 0;
-int halyytys = 0;
-
+int doorSensor = 0;    // Variable for door sensor
+int windowSensor = 0;  // Variable for window sensor
+int motionSensor = 0;  // Variable for motion sensor
+int alarm = 0;         // Variable for alarm status
 
 void setup() {
-  // initialize serial communications at 9600 bps:
+  // Initialize serial communications at 9600 bps:
   Serial.begin(9600);
 }
 
 void loop() {
-  // read the analog in value:
-  ovisensori = analogRead(analogInPin0); //luetaan AnalogInPin0 A0 pinniltä lukema ja tallennetaan se ovisensori muuttujalle
-  ikkunasensori  = analogRead(analogInPin1); //sama kuin edellä, mutta eri pinni ja muuttuja
-  liikesensori = analogRead(analogInPin2);
-  halyytys = analogRead(analogInPin3);
-  
+  // Read the analog in value:
+  doorSensor = analogRead(analogInPin0); // Read from pin A0 and store in doorSensor
+  windowSensor = analogRead(analogInPin1); // Read from pin A1 and store in windowSensor
+  motionSensor = analogRead(analogInPin2); // Read from pin A2 and store in motionSensor
+  alarm = analogRead(analogInPin3); // Read from pin A3 and store in alarm
 
-  // print the results to the Serial Monitor:
-  Serial.print(ovisensori /1023.0); //otetaan ovisensori-muuttujan arvo ja skaalataan se välille 0-1, eli jaetaan 1023:lla
-  Serial.print("\t  "); //tulostetaan lukujen väliin sarkain (tabulaattori), jotta tulokset tulevat samalle riville erikseen toisistaan
-  Serial.print(ikkunasensori /1023.0);
-  Serial.print("\t  ");
-  Serial.print(liikesensori /1023.0);
-  Serial.print("\t  ");
-  Serial.println(halyytys /1023.0); //huomaa tässä komennossa ln -pääte, joka tekee rivinvaihdon
+  // Print the results to the Serial Monitor:
+  Serial.print(doorSensor / 1023.0); // Scale doorSensor value to range 0-1
+  Serial.print("\t  "); // Print a tab for spacing
+  Serial.print(windowSensor / 1023.0); // Scale windowSensor value to range 0-1
+  Serial.print("\t  "); // Print a tab for spacing
+  Serial.print(motionSensor / 1023.0); // Scale motionSensor value to range 0-1
+  Serial.print("\t  "); // Print a tab for spacing
+  Serial.println(alarm / 1023.0); // Scale alarm value to range 0-1 and print with line break
 
-
-  // wait 20 milliseconds before the next loop for the analog-to-digital
+  // Wait 20 milliseconds before the next loop for the analog-to-digital
   // converter to settle after the last reading:
   delay(20);
 }
